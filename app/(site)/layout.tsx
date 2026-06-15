eb31dce7f68661597665473543db8978599d4def
Matt Phelps <matt@leandme.com>
2026-06-15 15:01:07 +0200

    Add Google Analytics tag

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import AmplitudeInitializer from "../components/AmplitudeInitializer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+const GOOGLE_ANALYTICS_ID = "G-B94X4749PN";
 
 export default function RootLayout({
   children,
@@ -11,7 +14,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-base-100 text-base-content">
-      <AmplitudeInitializer />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+        <AmplitudeInitializer />
         <Navbar />
         <main className="container mx-auto px-4 lg:px-8 py-8 min-h-screen">
           {children}
